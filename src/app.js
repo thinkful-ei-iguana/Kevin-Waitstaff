179dbe9071964994fac159e0e3e91e0e38e6cf97
Kevin Moore <moore.kevin.p@gmail.com>
2019-11-25 16:17:02 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let STORE = {
+  BasePrice: 0,
+  Tax: 0,
+  Tip: 0,
+  Total: 0,
+  Meals: 0,
+  TipTotal: 0
+};
+
+function handleForm() {
+  $('.meal-form').on('submit', event => {
+    event.preventDefault();
+    let formPrice = $('#price').val();
+    let formTax = $('#tax').val();
+    let formTip = $('#tip').val();
+    STORE.Meals++;
+    let newTotal = STORE.TipTotal;
+    STORE.TipTotal = newTotal + formTip;
+    STORE.BasePrice = formPrice;
+    STORE.Tax = formTax;
+    updateEarnings();
+    updateCharges();
+  });
+}
+
+function updateCharges() {
+  $('.subtotal').html(`<p>Subtotal: ${STORE.BasePrice + STORE.Tax}</p>`);
+  $('.tip').html(`<p>Tip: ${STORE.Tip}</p>`);
+  $('.total').html(`<p>Total: ${STORE.Total + STORE.Tip}</p>`);
+}
+
+function updateEarnings() {
+  $('.tip-total').html(`<p>Tip Total: ${STORE.TipTotal}</p>`);
+  $('.meal-count').html(`<p>Meal Count: ${STORE.Meals}</p>`);
+  $('.average-tip').html(`<p>Average Tip Per Meal: ${STORE.TipTotal / STORE.Meals}</p>`);
+}
+
+function handleReset() {
+  $('.reset').on('click', event => {
+    STORE.BasePrice = 0;
+    STORE.Tax = 0;
+    STORE.Tip = 0;
+    STORE.Total = 0;
+    STORE.Meals = 0;
+    updateCharges();
+    updateEarnings();
+  });
+}
+
+function main () {
+  handleForm();
+  handleReset();
+}
+
+$(main);
